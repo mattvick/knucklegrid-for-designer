20484bf3673ea43c38545650086cde390a4d69c2
Matthew <vickery.matthew@gmail.com>
2023-08-28 13:19:57 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,12 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      // Todo: revert this
+      // Use the default breakpoints and adjust the grid widths accordingly
+      'sm': '715px',
+      'md': '962px',
+    },
   },
   plugins: [],
 } satisfies Config;
